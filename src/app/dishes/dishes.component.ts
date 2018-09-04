7df86af8f6319f48bcb5750183ff98da1a31649e
TL <31960565+kredki@users.noreply.github.com>
2018-09-04 21:09:35 +0200

    list of dishes

--- src/app/dishes/dishes.component.ts
+++ src/app/dishes/dishes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { Dish } from '../dish';
+import { DISHES } from '../mock-dishes';
 
 @Component({
   selector: 'app-dishes',
@@ -7,14 +8,7 @@ import { Dish } from '../dish';
   styleUrls: ['./dishes.component.css']
 })
 export class DishesComponent implements OnInit {
-  dish: Dish = {
-    id: 1,
-    name: 'Pizza Margherita',
-    isAvailable: true,
-    description: 'Sos, ser',
-    type: 'pizza',
-    price: 22
-  };
+  dishes = DISHES;
 
   constructor() {
   }
