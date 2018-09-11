8b2a05ef765fb1ac6a01845020d74cff04c0833f
TL <31960565+kredki@users.noreply.github.com>
2018-09-11 16:25:00 +0200

    tests

--- src/app/login.service.spec.ts
+++ src/app/login.service.spec.ts
@@ -1,9 +1,11 @@
-import {inject, TestBed} from '@angular/core/testing';
+import {fakeAsync, inject, TestBed, tick} from '@angular/core/testing';
 
 import {LoginService} from './login.service';
 import {HttpClient, HttpHandler} from '@angular/common/http';
 import {RouterTestingModule} from '@angular/router/testing';
 import {FormsModule} from '@angular/forms';
+import {UserData} from './user-data';
+import {of} from 'rxjs';
 
 describe('LoginService', () => {
   beforeEach(() => {
@@ -16,4 +18,54 @@ describe('LoginService', () => {
   it('should be created', inject([LoginService], (service: LoginService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should check correct login', fakeAsync(() => {
+    inject([LoginService], (service: LoginService) => {
+      //given
+      const userData: UserData = new UserData();
+      userData.id = 1;
+      userData.name = 'admin';
+      userData.password = 'pass';
+      const observableUserData = of(userData);
+      const getSpy = spyOn(service.http, 'get').and.returnValue(observableUserData);
+
+      //when
+      service.checkLogin(userData.name, userData.password);
+
+      //then
+      expect(getSpy).toHaveBeenCalledWith('/api/users');
+      tick();
+      expect(service.adminLogged).toBeTruthy();
+      service.adminLogged = false;
+    });
+  )
+};
+
+  it('should check correct login', fakeAsync(() => {
+    inject([LoginService], (service: LoginService) => {
+      //given
+      const userData: UserData = new UserData();
+      userData.id = 1;
+      userData.name = 'admin';
+      userData.password = 'pass';
+
+      const userDataInDB: UserData = new UserData();
+      userDataInDB.id = 1;
+      userDataInDB.name = 'admin';
+      userDataInDB.password = 'pass';
+
+      const observableUserData = of(userDataInDB);
+      const getSpy = spyOn(service.http, 'get').and.returnValue(observableUserData);
+
+      //when
+      service.checkLogin(userData.name, userData.password);
+
+      //then
+      expect(getSpy).toHaveBeenCalledWith('/api/users');
+      tick();
+      expect(service.adminLogged).toBeFalsy();
+      service.adminLogged = false;
+    });
+  )
+};
 });
